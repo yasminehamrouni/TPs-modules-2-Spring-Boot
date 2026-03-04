dd61539db4071bcaa7594d98ec602b1009059230
yasminehamrouni <yasminetteham@gmail.com>
2026-03-04 06:29:15 +0100

    Ajout des fichiers non suivis

---
+++ tpSpringBoot-main/_ portail étudiant/src/main/java/com/example/portail/etudiant/PortailEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.example.portail.etudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailEtudiantApplication.class, args);
+	}
+
+}
---
+++ tpSpringBoot-main/spring-thymeleaf-lab/src/main/java/com/example/spring_thymeleaf_lab/SpringThymeleafLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_thymeleaf_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafLabApplication.class, args);
+	}
+
+}
